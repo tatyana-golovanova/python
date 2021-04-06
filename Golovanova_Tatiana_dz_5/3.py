b956e9e363e31a2c5e087489b8da5cb5bdb33b82
Tatyana <golovanova.ta0@gmail.com>
2021-04-06 12:34:03 +0500

    Готово третье задание.

---
+++ Golovanova_Tatiana_dz_5/3.py
@@ -0,0 +1,18 @@
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+]
+
+
+def gen_g(tutors_iter, klasses_iter):
+    for idx, tutor in enumerate(tutors_iter):
+        if idx < len(klasses_iter):
+            klass = klasses_iter[idx]
+        else:
+            klass = None
+        yield tutor, klass
+
+
+print(list(gen_g(tutors, klasses)))
